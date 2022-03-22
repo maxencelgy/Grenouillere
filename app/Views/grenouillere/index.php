ede6d60addee5d5206ea5ec213eea23d4da61365
maxencelgy <maxencelgy@gmail.com>
2022-03-22 16:17:16 +0100

    Main page

--- app/Views/grenouillere/index.php
+++ app/Views/grenouillere/index.php
@@ -8,8 +8,36 @@
 <?= $this->section('content') ?>
 
 <section id="main">
-    <div class="wrap">
-        
+    <div class="wrap2">
+        <img class="nenu" src="/asset/img/nenuphar.svg" alt="">
+        <div class="leftMain">
+
+            <h1>Grenouillere</h1>
+            <h2>Trouvez parmi plus de 2000 annonces <br> d’Assitant Martenelle ou de creche <br> qualifiées !</h2>
+            <form action="" class="filterSearch">
+                <div class="top">
+                    <select name="ville" id="ville">
+                        <option value="">ville</option>
+                    </select>
+                    <select name="enfant" id="enfant">
+                        <option value="">Nombre d'enfant</option>
+                    </select>
+                    <select name="Journée" id="Journée">
+                        <option value="">Journée</option>
+                    </select>
+                    <select name="Horraire" id="Horraire">
+                        <option value="">Horraire</option>
+                    </select>
+                </div>
+                <div class="bottom">
+                    <input type="submit" name="submitted" value="Rechercher">
+                </div>
+
+            </form>
+        </div>
+        <div class="rightMain">
+            <img src="/asset/img/bigFroag.svg" alt="">
+        </div>
     </div>
 </section>
 
