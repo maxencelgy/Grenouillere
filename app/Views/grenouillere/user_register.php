625a47c6afb9e3bd7d18dcdc16314601c5192d14
Romain THIERRY <romain_thierry2002@hotmail.fr>
2022-03-25 09:57:16 +0100

    style connexion/inscription particulier

--- app/Views/grenouillere/user_register.php
+++ app/Views/grenouillere/user_register.php
@@ -3,18 +3,22 @@
 <?= $validation->listErrors(); ?>
 
 <link rel="stylesheet" href="/asset/css/user_register.css">
-<form action="" method="post">
-    <input type="text" name="last_name_users" placeholder="Nom">
-    <span class="error"></span>
-    <input type="text" name="first_name_users" placeholder="Prenom">
-    <span class="error"></span>
-    <input type="text" name="email_users" placeholder="exemple@gmail.com">
-    <span class="error"></span>
-    <input type="password" name="password_users">
-    <span class="error"></span>
-    <input type="password" name="password_users_confirm">
-    <input type="checkbox">J’accepte la politique de confidentialité du site
 
-    <input type="submit" name="submitted" value="Envoyer">
+<h2>Particulier inscription</h2>
+
+<form action="" method="post" class="form">
+    <input type="text" name="last_name_users" placeholder="Nom" id="box-shadow">
+    <span class="error"></span>
+    <input type="text" name="first_name_users" placeholder="Prenom" id="box-shadow">
+    <span class="error"></span>
+    <input type="text" name="email_users" placeholder="exemple@gmail.com" id="box-shadow">
+    <span class="error"></span>
+    <input type="password" name="password_users" id="box-shadow" placeholder="Mot de passe">
+    <span class="error"></span>
+    <input type="password" name="password_users_confirm" id="box-shadow" placeholder="Confirmer votre mot de passe">
+    <div class="checkbox">
+        <input type="checkbox" id="checkbox"><p>J’accepte la politique de confidentialité du site</p>
+    </div>
+    <input type="submit" name="submitted" value="Envoyer" class="submit" id="box-shadow">
 </form>
 <?= $this->endSection() ?>
