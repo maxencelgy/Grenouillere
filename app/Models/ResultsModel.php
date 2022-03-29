dfdd17484e680c39502f66db32a473189800b48e
Pinto Jody <pintojody20@gmail.com>
2022-03-29 16:16:55 +0200

    :white_check_mark: geolocation

--- app/Models/ResultsModel.php
+++ app/Models/ResultsModel.php
@@ -14,7 +14,7 @@ class ResultsModel extends Model
     public function createJsonFile()
     {
         $sql = $this->select('name_company,city_company,postal_code_company,
-        adress_company,x_company,y_company')->findAll();
+        adress_company,x_company,y_company,hourly_rate_company')->findAll();
         $json = json_encode($sql);
         $jsonFile = file_put_contents("api_company.json", $json);
     }
