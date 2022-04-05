cc5b576d0180904e43e0dfdf21486c7db3e1a9fd
MaxenceBossin <maxbossin25@gmail.com>
2022-04-05 09:14:57 +0200

    wrong checkout

--- app/Views/templates/calendar/planning.php
+++ app/Views/templates/calendar/planning.php
@@ -1,13 +1,13 @@
 <section id="planning">
 
     <?php
-    if(!empty($slot)){
+    if (!empty($slot)) {
         // var_dump($slot);
         // var_dump($chidrenList);
         // var_dump (session()->get("id"));
         // var_dump ('eeere');
 
-    }    
+    }
     setlocale(LC_TIME, "fr_FR");
     ?>
     <div class="wrap">
@@ -20,13 +20,13 @@
                     <h2 class='date'><?= date('Y-m-d', strtotime("+ $i days")) ?></h2>
                     <div class="slots">
                         <?php
-                        
-                        foreach($planning as $day){
-                            // On boucle dans le tableau slot
-                            if(!empty($slot)){
-                                foreach($slot as $uniqueSlot){
-                                    if($uniqueSlot['fk_planning'] === $day['id_planning'] && $uniqueSlot['date_slot'] === strftime("%Y-%m-%d", strtotime("+ $i days")) && $uniqueSlot['child_remaining_slot'] > 0){
-                                        ?>
+
+                            foreach ($planning as $day) {
+                                // On boucle dans le tableau slot
+                                if (!empty($slot)) {
+                                    foreach ($slot as $uniqueSlot) {
+                                        if ($uniqueSlot['fk_planning'] === $day['id_planning'] && $uniqueSlot['date_slot'] === strftime("%Y-%m-%d", strtotime("+ $i days")) && $uniqueSlot['child_remaining_slot'] > 0) {
+                                            ?>
                         <div class="slot">
                             <div style="display: none;"><?= strftime("%Y-%m-%d", strtotime("+ $i days")) ?></div>
                             <div style="display: none;"> <?= $day['id_planning'] ?> </div>
@@ -35,25 +35,25 @@
                             <div> <?= $day['libelle_planning']  ?> </div>
                             <div> Nombre de place Disponible : <?= $uniqueSlot['child_remaining_slot'] ?> </div>
                         </div>
-                        <?php   
+                        <?php
+                                        }
                                     }
-                                }
-                            }else{
-                                ?>
+                                } else {
+                                    ?>
                         <div class="slot">
                             <div style="display: none;"><?= strftime("%Y-%m-%d", strtotime("+ $i days")) ?></div>
                             <div style="display: none;"> <?= $day['id_planning'] ?> </div>
                             <div> <?= $day['libelle_planning'] ?> </div>
                         </div>
                         <?php
-                            }                            
-                        }
-                        ?>
+                                }
+                            }
+                            ?>
                     </div>
                 </div>
             </div>
-            <?php         
-        } ?>
+            <?php
+            } ?>
         </div>
     </div>
 
@@ -63,19 +63,15 @@
         <div class=" wrap" id="response">
 
             <?php
-        if
-        ( !empty($slot) && !empty($chidrenList)){?>
-
-
+            if (!empty($slot) && !empty($chidrenList)) { ?>
             <?php
-                $i=0;
-            foreach( $chidrenList as $uniqueChild){ ?>
-            <div class='childs' id=" <?= $uniqueChild['id_child']?>"> <?= $uniqueChild['first_name_child']?>
+                $i = 0;
+                foreach ($chidrenList as $uniqueChild) { ?>
+            <div class='childs' id=" <?= $uniqueChild['id_child'] ?>"> <?= $uniqueChild['first_name_child'] ?>
             </div>
             <?php
-            }
-            
-        } ?>
+                }
+            } ?>
         </div> <input class="send" type="submit" value="<?= $infoBtn[1] ?>">
     </form>
 
