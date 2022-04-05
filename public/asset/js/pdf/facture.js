ac7035d699bbed7f3be4c057c2600fbc697fdd79
maxencelgy <maxencelgy@gmail.com>
2022-04-05 15:29:23 +0200

    Pdf fonctionnel

---
+++ public/asset/js/pdf/facture.js
@@ -0,0 +1,33 @@
+const options = {
+  margin: 0.5,
+  filename: "invoice.pdf",
+  image: {
+    type: "jpeg",
+    quality: 500,
+  },
+  html2canvas: {
+    scale: 1,
+  },
+  jsPDF: {
+    unit: "in",
+    format: "letter",
+    orientation: "portrait",
+  },
+};
+
+$(".btn-download").click(function (e) {
+  e.preventDefault();
+  const element = document.getElementById("invoice");
+  html2pdf().from(element).set(options).save();
+});
+
+function printDiv(divName) {
+  var printContents = document.getElementById(divName).innerHTML;
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+
+  window.print();
+
+  document.body.innerHTML = originalContents;
+}
