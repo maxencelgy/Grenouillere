1770a3ed92589ae2620b1d5e97bb20b727a88f32
maxencelgy <maxencelgy@gmail.com>
2022-04-05 14:10:49 +0200

    Popup company profil

--- app/Views/profil/profil_company.php
+++ app/Views/profil/profil_company.php
@@ -1,142 +1,189 @@
 <?= $this->extend('templates/default') ?>
 <?= $this->section('stylesheet') ?>
-    <link rel="stylesheet" href="/asset/css/profil.css">
+<link rel="stylesheet" href="/asset/css/profil.css">
 <?= $this->endSection() ?>
 
 
 <?= $this->section('content') ?>
-
+<div class="wrapProfil">
     <section id="profil">
-        <div class="wrapProfil">
-            <h2>Mes informations</h2>
-            <div class="informations">
-                <div class="information">
-                    <h3>Entreprise : <?= session()->get("name_company") ?></h3>
-                    <h3>Direction : <?= session()->get("frist_name_company") ?></h3>
-                    <h3>SIRET : </h3>
-                </div>
-                <div class="information">
-                    <h3>Email : <?= session()->get("email") ?></h3>
-                    <h3>Adresse : <?= session()->get("adress_company") ?></h3>
-                </div>
-                <div class="information">
-                    <h3>Ville : <?= session()->get("city_company") ?></h3>
-                    <h3>Code Postale : <?= session()->get("postal_code_company") ?></h3>
-                </div>
+        <h2>Mes informations</h2><br>
+        <div class="informations">
+            <div class="information">
+                <h3>Nom : <?= session()->get("nom") ?></h3><br>
+                <h3>Email : <?= session()->get("email") ?></h3><br>
+                <h3>Code Postale : <?= session()->get("postal") ?>27100</h3><br><br>
             </div>
+            <div class="information">
+                <h3>Prénom : <?= session()->get("prenom") ?></h3><br>
+                <h3>Adresse : <?= session()->get("adresse") ?>14 rue de la sauvagine</h3><br>
 
-            <div class="actu">
-                <a class="btn" href="<?= site_url(); ?>profil/editCompany">Mettre à jour mes informations</a>
             </div>
-
+            <div class="information">
+                <h3>Télephone : <?= session()->get("telephone") ?>0651718409</h3><br>
+                <h3>Ville : <?= session()->get("ville") ?>Val de reuil</h3><br>
+            </div>
         </div>
+        <div class="buttons">
+            <a class="btn" href="<?= site_url(); ?>profil/editCompany">Mettre à jour mes informations</a>
+        </div>
+    </section>
 
-        <div id="administration" class="wrapProfil">
-            <div class="administration_status">
-                <h2>Mes documents</h2>
-                <div class="card">
-                    <p>Pièce d'identité : </p>
-                        <?php if(!empty(session()->get("cni_company"))){ ?>
+    <section id="administration">
+        <h2>Mes documents</h2>
+        <div class="administration_status">
+            <div class="card">
+                <div class="box">
+                    <div class="threeBox">
+                        <h3>Pièce d'identité : </h3>
+                    </div>
+                    <div class="threeBox">
+                        <?php if (!empty(session()->get("cni_company"))) { ?>
                             <a href="<?= session()->get("cni_company") ?>">Voir le document</a>
-                        <?php }else{ ?>
+                        <?php } else { ?>
                             <p> EN ATTENTE </p>
                         <?php } ?>
-                    <a class="btn btn_identity_popup" href="">Mettre à jour</a>
+                    </div>
+                    <div class="threeBox">
+                        <a class="btnChild btn_identity_popup" href="">Mettre à jour</a>
+                    </div>
                 </div>
-                <div class="card">
-                    <p>Diplôme : </p>
-                    <?php if(!empty(session()->get("certificate_company"))){ ?>
-                        <a href="<?= session()->get("certificate_company") ?>">Voir le document</a>
-                    <?php }else{ ?>
-                        <p> EN ATTENTE </p>
-                    <?php } ?>
-                    <a class="btn btn_certificate_popup" href="">Mettre à jour</a>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <div class="threeBox">
+                        <h3>Diplôme : </h3>
+                    </div>
+                    <div class="threeBox">
+                        <?php if (!empty(session()->get("certificate_company"))) { ?>
+                            <a href="<?= session()->get("certificate_company") ?>">Voir le document</a>
+                        <?php } else { ?>
+                            <p> EN ATTENTE </p>
+                        <?php } ?>
+                    </div>
+                    <div class="threeBox">
+                        <a class="btnChild btn_certificate_popup" href="">Mettre à jour</a>
+                    </div>
                 </div>
-                <div class="card">
-                    <p>Autorisation préfectorale : </p>
-                    <?php if(!empty(session()->get("licence_company"))){ ?>
-                        <a href="<?= session()->get("licence_company") ?>">Voir le document</a>
-                    <?php }else{ ?>
-                        <p> EN ATTENTE </p>
-                    <?php } ?>
-                    <a class="btn btn_licence_popup" href="">Mettre à jour</a>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <div class="threeBox">
+                        <h3>Autorisation préfectorale : </h3>
+                    </div>
+                    <div class="threeBox">
+                        <?php if (!empty(session()->get("licence_company"))) { ?>
+                            <a href="<?= session()->get("licence_company") ?>">Voir le document</a>
+                        <?php } else { ?>
+                            <p> EN ATTENTE </p>
+                        <?php } ?>
+                    </div>
+                    <div class="threeBox">
+                        <a class="btnChild btn_licence_popup" href="">Mettre à jour</a>
+                    </div>
                 </div>
-                <div class="card">
-                    <p>Extrait Kbis : </p>
-                    <?php if(!empty(session()->get("kbis_company"))){ ?>
-                        <a href="<?= session()->get("kbis_company") ?>">Voir le document</a>
-                    <?php }else{ ?>
-                        <p> EN ATTENTE </p>
-                    <?php } ?>
-                    <a class="btn btn_kbis_popup" href="">Mettre à jour</a>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <div class="threeBox">
+                        <h3>Extrait Kbis : </h3>
+                    </div>
+                    <div class="threeBox">
+                        <?php if (!empty(session()->get("kbis_company"))) { ?>
+                            <a href="<?= session()->get("kbis_company") ?>">Voir le document</a>
+                        <?php } else { ?>
+                            <p> EN ATTENTE </p>
+                        <?php } ?>
+                    </div>
+                    <div class="threeBox">
+                        <a class="btnChild btn_kbis_popup" href="">Mettre à jour</a>
+                    </div>
                 </div>
-                <div class="card">
-                    <p>RIB : </p>
-                    <?php if(!empty(session()->get("rib_company"))){ ?>
-                        <a href="<?= session()->get("rib_company") ?>">Voir le document</a>
-                    <?php }else{ ?>
-                        <p> EN ATTENTE </p>
-                    <?php } ?>
-                    <a class="btn btn_rib_popup" href="">Mettre à jour</a>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <div class="threeBox">
+                        <h3>RIB : </h3>
+                    </div>
+                    <div class="threeBox">
+                        <?php if (!empty(session()->get("rib_company"))) { ?>
+                            <a href="<?= session()->get("rib_company") ?>">Voir le document</a>
+                        <?php } else { ?>
+                            <p> EN ATTENTE </p>
+                        <?php } ?>
+                    </div>
+                    <div class="threeBox">
+                        <a class="btnChild btn_rib_popup" href="">Mettre à jour</a>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 
 
+
     <!--Popup rib -->
 
-<div id="popup_container">
-    <form id="rib_popup" class="rib_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
-        <h2>Ajouter un rib</h2>
-        <label for="rib_company">RIB</label>
-        <input name="rib_company" type="file">
-        <input type="submit" value="Mettre à jour">
-    </form>
-</div>
+    <div class="filtre">
 
-<!-- Popup Identité -->
+        <div class="popup popup1">
+            <i class="fa-solid fa-xmark"></i>
+            <form class="rib_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
+                <h2>Ajouter un rib</h2>
+                <label for="rib_company">RIB</label>
+                <input name="rib_company" type="file">
+                <input type="submit" value="Mettre à jour">
+            </form>
+        </div>
+    </div>
 
-<div id="popup_container">
-    <form id="identity_popup" class="identity_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
-        <h2>Ajouter une pièce d'identité</h2>
-        <label for="identity_company">Pièce d'identié :</label>
-        <input name="identity_company" type="file">
-        <input type="submit" value="Mettre à jour">
-    </form>
-</div>
+    <!-- Popup Identité -->
 
-<!-- Popup Diplome -->
+    <div class="popup popup2">
+        <i class="fa-solid fa-xmark"></i>
+        <form class="identity_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
+            <h2>Ajouter une pièce d'identité</h2>
+            <label for="identity_company">Pièce d'identié :</label>
+            <input name="identity_company" type="file">
+            <input type="submit" value="Mettre à jour">
+        </form>
+    </div>
 
-<div id="popup_container">
-    <form id="certificate_popup" class="certificate_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
-        <h2>Ajouter un diplôme :</h2>
-        <label for="certificate_company">Diplôme :</label>
-        <input name="certificate_company" type="file">
-        <input type="submit" value="Mettre à jour">
-    </form>
-</div>
+    <!-- Popup Diplome -->
 
-<!-- Popup licence  -->
+    <div class="popup popup3">
+        <i class="fa-solid fa-xmark"></i>
+        <form class="certificate_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
+            <h2>Ajouter un diplôme :</h2>
+            <label for="certificate_company">Diplôme :</label>
+            <input name="certificate_company" type="file">
+            <input type="submit" value="Mettre à jour">
+        </form>
+    </div>
 
-<div id="popup_container">
-    <form id="licence_popup" class="licence_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
-        <h2>Ajouter une autorisation préfectorale :</h2>
-        <label for="licence_company">Attestation :</label>
-        <input name="licence_company" type="file">
-        <input type="submit" value="Mettre à jour">
-    </form>
-</div>
+    <!-- Popup licence  -->
 
-<!-- Popup Kbis  -->
+    <div class="popup popup4">
+        <i class="fa-solid fa-xmark"></i>
+        <form class="licence_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
+            <h2>Ajouter une autorisation préfectorale :</h2>
+            <label for="licence_company">Attestation :</label>
+            <input name="licence_company" type="file">
+            <input type="submit" value="Mettre à jour">
+        </form>
+    </div>
 
-<div id="popup_container">
-    <form id="kbis_popup" class="kbis_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
-        <h2>Ajouter un Kbis :</h2>
-        <label for="kbis_company">Kbis :</label>
-        <input name="kbis_company" type="file">
-        <input type="submit" value="Mettre à jour">
-    </form>
+    <!-- Popup Kbis  -->
+
+    <div class="popup popup5">
+        <i class="fa-solid fa-xmark"></i>
+        <form class="kbis_popup popup_hidden" action="<?= site_url(); ?>add/addFiles/<?= session()->get("id") ?>" enctype="multipart/form-data" method="post">
+            <h2>Ajouter un Kbis :</h2>
+            <label for="kbis_company">Kbis :</label>
+            <input name="kbis_company" type="file">
+            <input type="submit" value="Mettre à jour">
+        </form>
+    </div>
 </div>
 
 
--- public/asset/js/popup.js
+++ public/asset/js/popup.js
@@ -6,8 +6,13 @@ const allergyButton = document.querySelectorAll(".btn_allergy_popup");
 const allergyPopup = document.querySelector(".child_allergy");
 const filtre = document.querySelector(".filtre");
 const croix = document.querySelectorAll(".fa-xmark");
+
 const popup = document.querySelector(".popup");
-const popup2 = document.querySelector(".popup2");
+
+const popup1 = document.querySelector(".popup1");
+
+const popup3 = document.querySelector(".popup3");
+const popup4 = document.querySelector(".popup4");
 
 allergyButton.forEach((e) => {
   e.addEventListener("click", function (e) {
@@ -21,6 +26,7 @@ allergyButton.forEach((e) => {
 });
 
 //Popup Maladie
+const popup2 = document.querySelector(".popup2");
 const diseaseButton = document.querySelectorAll(".btn_disease_popup");
 const diseasePopup = document.querySelector(".child_disease");
 
@@ -35,35 +41,43 @@ diseaseButton.forEach((e) => {
   });
 });
 
+croix.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("cc");
+    filtre.style.display = "none";
+    popup2.style.display = "none";
+    popup.style.display = "none";
+  });
+});
+
 //////////////////Popup function/////////////////////////////
 
-function popupFolder(btnClass, popupClass) {
+function popupFolder(btnClass, popupClass, form) {
   const popupBtn = document.querySelector("." + btnClass);
   const popupContainer = document.querySelector("." + popupClass);
+  const popupForm = document.querySelector("." + form);
 
   popupBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    popupContainer.className = ".popup_display";
+    popupContainer.style.display = "block";
+    filtre.style.display = "block";
+    popupForm.className = ".popup_display";
     popupContainer.innerHTML += `<input style="display: none" type="text" name="" value="${this.id}">`;
   });
 }
-croix.forEach((element) => {
-  element.addEventListener("click", function (e) {
-    filtre.style.display = "none";
-    popup2.style.display = "none";
-  });
-});
+
 //Popup RIB
-popupFolder("btn_rib_popup", "rib_popup");
+popupFolder("btn_rib_popup", "popup1", "rib_popup");
 
 //Popup identity
-popupFolder("btn_identity_popup", "identity_popup");
+popupFolder("btn_identity_popup", "popup2", "identity_popup");
 
 //Popup certificate
-popupFolder("btn_certificate_popup", "certificate_popup");
+popupFolder("btn_certificate_popup", "popup3", "certificate_popup");
 
 //Popup Licence
-popupFolder("btn_licence_popup", "licence_popup");
+popupFolder("btn_licence_popup", "popup4", "licence_popup");
 
 //Popup Kbis
-popupFolder("btn_kbis_popup", "kbis_popup");
+popupFolder("btn_kbis_popup", "popup5", "kbis_popup");
