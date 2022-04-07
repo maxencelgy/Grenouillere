63c751e617785b9c54d6f4e58925974b0b36c612
Kapeupro <dimitri.sarrazin.pro.dev@gmail.com>
2022-04-07 17:09:48 +0200

    added text in private life

--- app/Views/legal/mentionLegal.php
+++ app/Views/legal/mentionLegal.php
@@ -9,32 +9,47 @@
     </div>
 
     <h3>Editeur</h3>
-    <p>Le site web bonjour.tousanticovid.gouv.fr sous la responsabilité éditoriale de la Direction Générale de la Santé du ministère des solidarités et de la santé, dont le siège est situé 14 avenue Duquesne 75350 Paris cedex 07.</p>
+    <p>
+        Grenouillere est un site internet fictif créé par Grenouillere.
+    </p>
 
     <h3>Directeur de publication</h3>
-    <p>Le directeur de la publication du site est Patrick BRASSEUR- chef de la mission communication de la Direction Générale de la Santé - 14, avenue Duquesne - 75350 Paris cedex 07</p>
+    <p>
+        L'entreprise Grenouillere est située à: Rouen.
+    </p>
 
     <h3>Objectif du site</h3>
-    <p>Le site a pour mission de mettre à disposition les liens de téléchargement vers l'application TousAntiCovid et toutes les informations entourant son fonctionnement.</p>
+    <p>
+        Rechercher des annonces de nounou pour les enfants ou de creche pour les enfants de moins de 3 ans.
+    </p>
 
     <h3>Développement</h3>
-    <p>Le développement du site web est réalisé par la société Need For School 43 rue beaubourg 76000 Rouen</p>
+    <p>
+        Le site Grenouillere est développé par Grenouillere.
+    </p>
 
     <h3>Hébergement</h3>
     <p>L'hébergement des serveurs est assuré par 3DS Outscale, 1, rue Royale – 319 Bureaux de la Colline 92210 Saint-Cloud.</p>
 
     <h3>Accès au service</h3>
-    <p>Le service est accessible gratuitement, 24h/24, 7 jours sur 7 et sauf cas de force majeure ou maintenance technique depuis tout équipement numérique en capacité d’afficher du contenu web (HTML/CSS/PHP).</p>
+    <p>
+        L'accès au service Grenouillere est assuré par Grenouille (3DS Outscale, 1, rue Royale – 319 Bureaux de la Colline 92210 Saint-Cloud).
+    </p>
 
-    <p>Sachez toutefois que la Direction Générale de la Santé a demandé au sous-traitant en charge de développer le service de prendre soin de vérifier que les critères indiqués dans le référentiel général d’amélioration de l’accessibilité (RGAA) soient appliqués le plus tôt possible afin de rendre le site internet accessible au plus grand nombre et plus particulièrement aux personnes ayant un handicap.</p>
+    <p>
+        Les informations recueillies par Grenouillere sont destinées à Grenouille.
+    </p>
 
     <h3>Accessibilité du service</h3>
-    <p>Au moment où nous rédigeons ces mentions légales, le site ne peut être déclaré conforme au RGAA version 4.</p>
-    <p>Sachez toutefois que la Direction Générale de la Santé a demandé au sous-traitant en charge de développer le service de prendre soin de vérifier que les critères indiqués dans le référentiel général d’amélioration de l’accessibilité (RGAA)soient appliqués le plus tôt possible afin de rendre le site internet accessible au plus grand nombre et plus particulièrement aux personnes ayant un handicap.</p>
+    <p>
+        Grenouillere est accessible 24h/24, 7j/7.
+    </p>
 
     <h3>Réutilisation des contenus</h3>
     <h3>Consignes générales</h3>
-    <p>Sauf mention contraire indiquée dans les partie ci-dessous, tout utilisation de contenu du site web par un utilisateur est autorisé et gratuite dès lors qu’il respecte les conditions suivantes :</p>
+    <p>
+        Grenouillere ne saurait être tenue pour responsable des contenus mis en ligne par les utilisateurs.
+    </p>
 
     <p>Ne pas modifier ou altérer d’aucune sorte le document reproduit ;</p>
     <p>Mentionner explicitement la source en indiquant « Source : Direction Générale de la Santé – suivi du lien vers le contenu d’origine » ;</p>
