79fa5f65594b47a4d5352b28c26b733418734b7a
maxencelgy <maxencelgy@gmail.com>
2022-04-07 17:47:37 +0200

    FINAL PUSH WITH 404 de DIMITRI

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,9 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override();
+$routes->set404Override(function () {
+    return view('/errors/html/error_404');
+});
 $routes->setAutoRoute(true);
 
 /*
